bbdd0a2e1f325e9986441890975ca63f139b66b9
ap0piec <erik.rakuscek@gmail.com>
2020-04-07 14:02:18 +0200

    Added cell data fetching

---
+++ statisticalShapeModel.py
@@ -0,0 +1,22 @@
+import quilt3
+
+import json
+
+# Fetching cell data #
+pkg = quilt3.Package.browse("aics/pipeline_integrated_single_cell", registry="s3://allencell")
+cells = pkg["cell_features"]
+# cells.fetch("cells")
+
+i = 0
+for cell in cells:
+
+    f = open("cells/" + cell)
+    cell_data = json.load(f)
+    f.close()
+
+    for (k, v) in cell_data.items():
+        print(v)
+
+    if i == 10:
+        break
+    i += 1
